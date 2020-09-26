bc8765e31bddd3008684535d142eea72cd7c7f31
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-09-25 19:41:36 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,5 @@
-function process(data) {
-    let samples = data.map(sample => sample.id);
-    console.log(metadata);
-    return metadata;
-}
-
-d3.json("../docs/samples.json").then(process);
-
-
-// Fetch the JSON data and console log it 
-// d3.json("../docs/samples.json").then((bacteriaData) => {
-//     console.log(bacteriaData)
-//     // let data = bacteriaData
-// });
-// //  or this way
-// d3.json("../docs/samples.json").then(bacteriaData => {
-//     console.log(bacteriaData)
-//     // let data = bacteriaData
-// });
-
-
+// 
+d3.json("samples.json").then((data) => {
+    let metadata = data.metadata;
+      console.log(data) // 
+    });
