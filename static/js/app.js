4d305ad5a655d11761212f9cbafde129860527d1
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-09-25 20:16:03 -0500

    updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,23 @@
 // 
+// d3.json("samples.json").then((data) => {
+//     let metadata = data.metadata;
+//       console.log(data) // 
+//     });
+
+// create a bar chart
 d3.json("samples.json").then((data) => {
-    let metadata = data.metadata;
-      console.log(data) // 
+    let samples = data.samples;
+    //   console.log(samples) 
+    //   bd = bacteria data   
+      let sampleValues = samples.map(bd => bd.sample_values);
+      let otuIds = samples.map(bd => bd.otu_ids);
+      let otuLabels = samples.map(bd => bd.otu_labels);
+      let sampleIds = samples.map(bd => bd.id);
+
+      console.log(sampleValues);
+      console.log(otuIds);
+      console.log(otuLabels);
+      console.log(sampleIds);
+
+
     });
