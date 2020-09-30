314383fc604be49e6ac12a673b2411a7f1e32b71
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-09-29 20:09:34 -0500

    updates

--- static/js/app1.js
+++ static/js/app1.js
@@ -1,17 +1,19 @@
-// function buildMetadata(sample){
-//     d3.json("../docs/samples.json").then((data) => {
-//         // let metadata = data.metadata; 
-//         console.log(metadata);
-// });
+
 function createMetadata(sample) {
     // read the json file and console log to make sure it works
-d3.json("../docs/samples.json").then((data) => {
+    d3.json("../docs/samples.json").then((data) => {
     let metadata = data.metadata;
     // console.log(data); 
-});
+    });
+    // filter results of the desire items
+    let resultMetadata = metadata.filter(sampleItem => sampleItem.id == sample);
+    let result= resultMetadata[0];
 
+    let dashbord = d3.select("#sample-metadata");
+    // clear any existing metadata
+    dashbord.html("");
 
-
-
-
+    Object.defineProperties(result).forEach(([key, value]) => {
+        dashbord.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
 };
